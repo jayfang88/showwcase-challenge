34b7e2c3a46970bf4d4607b1a1e70ac426dca23e
jayfang88 <jayfang88@gmail.com>
2020-09-14 12:22:10 -0700

    update styling to home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import './reset.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="main">
+        <p>Hi there! Welcome to your education showcase.</p>
+        <p>Type your name and click "Enter" below to begin!</p>
+        <input type="text" className='name-input'></input>
+        <button className='name-button' onClick={() => console.log('hi')}>Enter</button>
+      </main>
     </div>
   );
 }
