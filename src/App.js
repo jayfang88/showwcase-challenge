3b2710e0dc3f766ab9e5683cf21345c9a4e60eb2
jayfang88 <jayfang88@gmail.com>
2020-09-14 12:39:32 -0700

    add input functionality

--- src/App.js
+++ src/App.js
@@ -2,17 +2,38 @@ import React from 'react';
 import './reset.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <main className="main">
-        <p>Hi there! Welcome to your education showcase.</p>
-        <p>Type your name and click "Enter" below to begin!</p>
-        <input type="text" className='name-input'></input>
-        <button className='name-button' onClick={() => console.log('hi')}>Enter</button>
-      </main>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.update = this.update.bind(this);
+  }
+
+  update(e) {
+    this.name = e.currentTarget.value;
+  }
+
+  goToMain() {
+    if (!this.name) {
+      alert('please enter a name');
+      return
+    }
+    
+    console.log(this.name);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <main className="main">
+          <p>Hi there! Welcome to your education showcase.</p>
+          <p>Type your name and click "Enter" below to begin!</p>
+          <input type="text" onChange={this.update} placeholder='Your Name' className='name-input'
+            value={this.name}></input>
+          <button className='name-button' onClick={() => this.goToMain()}>Enter</button>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
