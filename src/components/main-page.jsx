3119ff394591192d4f332c4bf5f713d9b853dd41
jayfang88 <jayfang88@gmail.com>
2020-09-16 13:50:39 -0700

    change Main to functional component for hooks

--- src/components/main-page.jsx
+++ src/components/main-page.jsx
@@ -1,53 +1,87 @@
 import React from 'react';
+import Modal from 'react-modal';
 import SchoolInfoCard from './school_info_card.jsx';
 
-class Main extends React.Component {
-    constructor() {
-        super();
-        this.education = [
-            {name: 'UC Berkeley', degree: 'BA', field: 'Psychology', start: '2012', end: '2016', gpa: '3.5', description: 'nice school'},
-            {name: 'App Academy', degree: 'Certificate', field: 'Full Stack Software Engineering', start: '2019', end: '2020', gpa: 'N/A', description: 'coding bootcamp'},
-        ];
+Modal.setAppElement('#root')
+
+const Main = (props) => {
+    let education = [
+        {name: 'UC Berkeley', degree: 'BA', field: 'Psychology', start: '2012', end: '2016', gpa: '3.5', description: 'nice school'},
+        {name: 'App Academy', degree: 'Certificate', field: 'Full Stack Software Engineering', start: '2019', end: '2020', gpa: 'N/A', description: 'coding bootcamp'},
+    ];
+
+    let name;
+    if (props.location.state && props.location.state.name) {
+        name = props.location.state.name;
+    } else {
+        name = 'No Name';
     }
 
-    componentDidMount() {
-        // if (this.props.location.state) {
-        //     this.name = this.props.location.state.name;
-        // } else {
-        //     this.name = 'No Name';
-        // }
-        // debugger;
-    }
-    
-
-    render() {
-        let name;
-        if (this.props.location.state && this.props.location.state.name) {
-            name = this.props.location.state.name;
-        } else {
-            name = 'No Name';
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)'
         }
+    };
+    const [modalIsOpen, setIsOpen] = React.useState(false);
 
-        let titles = this.education.map((school, i) => <li key={i} className='edu-li'>{school.name}</li>);
-        let schools = this.education.map((school, i) => <SchoolInfoCard school={school} key={i}/>)
-
-        return (
-            <div className='main'>
-                <header className='main-header'>
-                    <h2>Welcome to <span className='bold'>{name}'s</span> education page.</h2>
-                    <button className='add-education'>Add new education</button>
-                </header>
-                <main className='main-body'>
-                    <aside className='main-aside'>
-                        <p className='main-aside-info'>{titles}</p>
-                    </aside>
-                    <section className='main-section'>
-                        <div className='main-section-info'>{schools}</div>
-                    </section>
-                </main>
-            </div>
-        )
+    function openModal() {
+        setIsOpen(true);
     }
+    function afterOpenModal() {
+        // subtitle.style.color = '#f00';
+    }
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    let titles = education.map((school, i) => <li key={i} className='edu-li'>{school.name}</li>);
+    let schools = education.map((school, i) => <SchoolInfoCard school={school} key={i}/>)
+
+    function addEducation(name, degree, field, start, end, gpa, description) {
+        education.unshift({
+            name: `${name}`,
+            degree: `${degree}`,
+            field: `${field}`,
+            start: `${start}`,
+            end: `${end}`,
+            gpa: `${gpa}`,
+            description: `${description}`
+        });
+    }
+
+    return (
+        <div className='main'>
+            <header className='main-header'>
+                <h2>Welcome to <span className='bold'>{name}'s</span> education page.</h2>
+                <button className='add-education' onClick={openModal}>Add new education</button>
+                <Modal 
+                    isOpen={modalIsOpen}
+                    onAfterOpen={afterOpenModal}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                >
+                    <h2>Dis the modal</h2>
+                    <form>
+                        <input type="text"/>
+                    </form>
+                </Modal>
+            </header>
+            <main className='main-body'>
+                <aside className='main-aside'>
+                    <p className='main-aside-info'>{titles}</p>
+                </aside>
+                <section className='main-section'>
+                    <div className='main-section-info'>{schools}</div>
+                </section>
+            </main>
+        </div>
+    )
+    
 }
 
 export default Main;
